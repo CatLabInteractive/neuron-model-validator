f3ea23f26c3e8f27f6cc667cccaf2eb29522879b
Thijs Van der Schaeghe <thijs@catlab.be>
2015-01-11 16:03:05 +0100

    More base setup.

--- src/CatLab/Validator/Validator.php
+++ src/CatLab/Validator/Validator.php
@@ -3,9 +3,23 @@ namespace CatLab\Validator;
 
 class Validator {
 
+	/**
+	 * Set up a validation environment based on swagger documentation.
+	 * @param $path
+	 */
 	public static function fromSwagger ($path)
 	{
 
+	}
+
+	public function __construct ()
+	{
+
+	}
+
+	public function validate ($modelName, $data)
+	{
+		return true;
 	}
 
 }
---
+++ tests/ValidatorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CatLab\Validator\Tests;
+
+use CatLab\Validator\Models\Model;
+use CatLab\Validator\Validator;
+
+class ValidatorTest
+	extends \PHPUnit_Framework_TestCase {
+
+	public function testValidator ()
+	{
+		$input = array (
+			'name' => 'This is my name',
+			'counter' => 15
+		);
+
+		$validator = new Validator ();
+
+		$model = new Model ('testModel');
+
+
+		$this->assertEquals (true, $validator->validate ($model, $input));
+	}
+
+}
